4067d4177aec7f1d51dc20ba19122b9a03bfe1c6
Ian Chege <ianchege001@gmail.com>
2023-04-12 18:46:11 +0300

    Display a list of emoji

--- App.js
+++ App.js
@@ -8,10 +8,12 @@ import ImageViewer from './components/ImageViewer';
 import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [pickedEmoji, setPickedEmoji] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
@@ -70,7 +72,8 @@ export default function App() {
         </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        {/* A list of emoji component will go here */}
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}>
+        </EmojiList>
       </EmojiPicker>
       <StatusBar style="auto" />
     </View>
